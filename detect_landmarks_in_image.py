d2338216bda385c49ad1c02496d081168c5ff050
Psychofun <36560929+Psychofun@users.noreply.github.com>
2019-08-14 14:51:38 -0500

    Test sunglasses filter.
    **Day 48** of #60daysofudacitychallenge.
    Add sunglasses filter. Needs transparency.

--- detect_landmarks_in_image.py
+++ detect_landmarks_in_image.py
@@ -11,7 +11,8 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-
+filters = ['images/sunglasses.png', 'images/sunglasses_2.png', 'images/sunglasses_3.jpg', 'images/sunglasses_4.png', 'images/sunglasses_5.jpg', 'images/sunglasses_6.png','images/pixel.png']
+filterIndex = 6
 
 def open_image(image_path):
     
@@ -39,89 +40,19 @@ def open_image_cv2(image_path):
 
     return img
 
-def plot_sample(x, y, axis):
-    img = x.reshape(96, 96)
-    axis.imshow(img, cmap='gray')
-    axis.scatter(y[0::2] * 48 + 48, y[1::2] * 48 + 48, marker='x', s=10)
 
-
-
-# Overlay a transparent image on background.
-def transparentOverlay(src , overlay , pos=(0,0),scale = 1):
-    overlay = cv2.resize(overlay,(0,0),fx=scale,fy=scale)
-    h,w,_ = overlay.shape  # Size of foreground
-    rows,cols,_ = src.shape  # Size of background Image
-    y,x = pos[0],pos[1]    # Position of foreground/overlay image
-    
-    #loop over all pixels and apply the blending equation
-    for i in range(h):
-        for j in range(w):
-            if x+i >= rows or y+j >= cols:
-                continue
-            alpha = float(overlay[i][j][3]/255.0) # read the alpha channel 
-            src[x+i][y+j] = alpha*overlay[i][j][:3]+(1-alpha)*src[x+i][y+j]
-    return src    
-    
-def get_filter_spots(preds, scale_x, scale_y, scale, dog_filter):    
-    #scale_x and y for scale of 96x96 image and scale for  overall filter scaling
-    filter_nose = dog_filter['nose']
-    filter_right_ear = dog_filter['ear_right']
-
-    #Hyper-Paramaters
-    y_padding = 5
-    ear_padding = 6
-
-
-
-    #Add nose
-    nose_x = preds[PRED_TYPES['nose'].slice,0][1]
-    nose_y = preds[PRED_TYPES['nose'].slice,1][1]
-
-    print("Nose x {} and y {}".format(nose_x,nose_y))
-
-    #exit()
-    nose_x = int(nose_x - filter_nose.shape[1]*scale/2)
-    nose_y = int( (nose_y + y_padding)*scale_y - filter_nose.shape[0]*scale/2)
-    
-
-    #result = transparentOverlay(img_crop.copy(),filter_nose,(x,y),scale)
-    
-    left_ear_x = 0 - ear_padding
-    left_ear_y = 0 - ear_padding*2
-    
-    right_ear_x = int( (ear_padding*2)*scale_x - filter_right_ear.shape[0]*scale)
-    right_ear_y = (0 - ear_padding)*scale_y
-    
-    return [nose_x, nose_y],[left_ear_x*scale_x, left_ear_y*scale_y],[right_ear_x, right_ear_y]
-
-def get_best_scaling(w):
-    filter_width = 420
-    return 1.1*(w/filter_width)
-    
-def get_eye_angle(pred):
-    left = pred[0:1]
-    right = pred[2:3]
-    #find vector
-    vec = [right[0]-left[0], right[1]-left[1] ]
-    angle = vec[0] / (math.sqrt(vec[0]*vec[0] + vec[1]*vec[1]))
-    
-    return angle
     
 # Main Function to apply dog filter
-def add_dog_filter(img_path, dog_filter):
+def add_filter(img_path, dog_filter):
     
 
-    img = open_image(img_path)
+    img = open_image_cv2(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     faces, preds = get_features_from_image(img)
+    #print("PREDS",preds)
 
 
-
-    filter_nose = dog_filter['nose']
-    filter_left_ear = dog_filter['ear_left']
-    filter_right_ear = dog_filter['ear_right']
-    
     if len(faces)==0:
         print("No faces Detected in the image")
         return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -132,27 +63,60 @@ def add_dog_filter(img_path, dog_filter):
         x,y,w,h = tuple(map(int, [x,y,w,h] ))
         # add bounding box to color image
         #cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        
-        img_crop = img[y:y+h, x:x+w]
-        
-        scale_x = 1 #img_crop.shape[0]/img.shape[0]
-        scale_y = 1 #img_crop.shape[1]/img.shape[1]
-        print("Scale x {}, y {}".format(scale_x, scale_y))
-        
-        scale = get_best_scaling(w)
-        print("Scale", scale)
-        
-        nose,left_ear,right_ear = get_filter_spots(preds, scale_x, scale_y, scale, dog_filter)  
-        #nose, left_ear,rigth_ear = 
 
-        #Add images
-        result = transparentOverlay(img.copy(),filter_nose,( int(nose[0]+x), int(nose[1]+y)), scale)
-        result = transparentOverlay(result.copy(),filter_left_ear,( int(left_ear[0]+x), int(left_ear[1]+y)), scale)
-        result = transparentOverlay(result.copy(),filter_right_ear,( int(right_ear[0]+x), int(right_ear[1]+y)), scale)
+        gray_face = gray[y: y + h,x: x + w]
+        color_face = img[y: y + h,x: x + w]
         
-        img = result
+
+        eye1, eye2 = (preds[PRED_TYPES['eye1'].slice,0],preds[PRED_TYPES['eye1'].slice,1]),(preds[PRED_TYPES['eye2'].slice,0],preds[PRED_TYPES['eye1'].slice,1])
+
+       
+
+        
+        sunglasses = cv2.imread(filters[filterIndex], cv2.IMREAD_UNCHANGED)
+
+        y_coordinates = eye1[1] + eye2[1]
+
+        max_y,min_y = np.max(y_coordinates), np.min(y_coordinates) 
+        sunglass_width = int(  max_y  -  min_y  ) *2
+
+
+        x_coordinates =  eye1[0] + eye2[0]
+        max_x,min_x =np.max(x_coordinates) , np.min(x_coordinates)
+
+
+        sunglass_heigth = int( max_x - min_x   ) *2
+        print("Eye 1 {}, Eye 2 {}".format(eye1,eye2))
+
+
+        print("Sunglasse width {} and height {} ".format(sunglass_width, sunglass_heigth )) 
+
+        sunglass_resized = cv2.resize(sunglasses, (sunglass_heigth,sunglass_width),interpolation = cv2.INTER_CUBIC)
+        transparent_region = sunglass_resized[:,:,:3] != 0 
+        
+        #Overlay sunglasses over img
+        #face_resized_color[int(points[9][1]):int(points[9][1])+sunglass_height, int(points[9][0]):int(points[9][0])+sunglass_width,:][transparent_region] = sunglass_resized[:,:,:3][transparent_region]
+        x_,y_ = int(np.min(eye1[0])),int(np.min(eye1[1]))
+        img[  y_ : y_ +sunglass_width,x_: x_ + sunglass_heigth:] = sunglass_resized[:,:,:3]
+        
+        #transparent_region_2 = sunglasses[:,:,:3] != 0 
+
+        #alpha = sunglasses[:, :, 3] / 255.0
+        #new_image = np.dstack((alpha * sunglasses[:,:,0], alpha * sunglasses[:,:,1],alpha * sunglasses[:,:,2]))
+        #img[0:sunglasses.shape[0]  ,0:sunglasses.shape[1] ,:] = new_image
+
+
+        #for point in preds:
+        #    cv2.circle(img, tuple(point), 1, (255,255,255), 1)
+        
+        # Test anchor point for sunglasses
+        #cv2.circle(img, (x_,y_), 2, (255,0,0),2)
+    #cv2.imshow("Filters", img)
+
+
+       
     #Change to RGB
-    result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+    result = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     #and return
     return result
     
@@ -262,10 +226,9 @@ if __name__ == "__main__":
 
     file_path = 'john_wick.jpg'
     #image_2d_landmarks(file_path)
-    result = add_dog_filter(file_path, dog_filter)
+    result = add_filter(file_path, dog_filter)
 
     plt.axis('off')
     plt.imshow(result)
     plt.show()
-    #result = cv2.cvtColor(result, cv2.COLOR_RGB2BGR)
-    #cv2.imwrite('Images/Edited/'+img_path[7:] ,result)
+    
